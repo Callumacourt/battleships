a653022699f0ca968551cf42360c3a0fda2510a4
Callum <callumacourtt@gmail.com>
2024-03-07 20:02:12 +0000

    Created ship place test

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,5 @@
+import Ship from './ship';
+
 export class gameboard {
   constructor() {
     this.rows = 10;
@@ -8,15 +10,26 @@ export class gameboard {
 
   createGameboard() {
     const letters = 'abcdefghij';
+    const createCell = (coordinates, occupied = null) => {};
 
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = new Array(this.columns);
 
       for (let j = 0; j < this.columns; j++) {
-        this.board[i][j] = `${letters[i]}${j + 1}`;
+        {
+          this.board[i][j] = [`${letters[i]}${j + 1}`, (this.occupied = false)];
+        }
       }
     }
 
     console.log(this.board);
+  }
+
+  placeShip(coordinates, shipSize, orientation) {
+    const cordCopy = new Array(coordinates);
+
+    // are cords occupied?
+    // if not, occupy cords according to ship size and orientation
+    // record cords as occupied
   }
 }
